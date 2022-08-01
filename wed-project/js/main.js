4637f33df935d4b85093e7f0fd2a2871930c5a13
Nguyễn Văn Hiếu <104955838+hieunguyenyt173@users.noreply.github.com>
2022-08-01 15:05:07 +0700

    update

--- wed-project/js/main.js
+++ wed-project/js/main.js
@@ -1,3 +1,74 @@
+// Menu-reponsive
+$(".menu-bar").click(function() {
+  $(".sidebar").css("left","0");
+  $(".overlay").css("display","block");
+  $('body').css('overflow', 'hidden');
+  
+})
+// close btn
+$(".close-icon").click(function() {
+  $(".sidebar-brand-women").css("left","-360px");
+  $(".sidebar-women").css("left","-360px");
+  $(".sidebar-brand").css("left","-360px");
+  $(".sidebar-men").css("left","-360px");
+  $(".sidebar").css("left","-360px");
+  $(".overlay").css("display","none");
+  $(".menu-men-desktop").css("top","-70%");
+  $(".subnav .menu-item:nth-child(2)").css("background-color", "#E12222")
+  $('body').css('overflow', 'auto');
+})
+
+// Menu-men-desktop
+$(".subnav .menu-item:nth-child(2)").click(function() {
+  $(".subnav .menu-item:nth-child(2)").css("background-color", "#000")
+  $(".menu-men-desktop").css("top","100px");
+  $(".overlay").css("display","block");
+  $(".overlay").css("top","100px");
+  $('body').css('overflow', 'hidden');
+})
+
+$(window).resize(function() {
+  if($(window).innerWidth > 768) {
+      $(".sidebar").css("left", "-360px");
+      $(".overlay").css("display","none");
+  }
+})
+
+// Sidebar-men
+$(".menu-men").click(function() {
+  $(".sidebar-men").css("left","0");
+  $(".overlay").css("display","block");
+  
+})
+
+$(".menu-women").click(function() {
+  $(".sidebar-women").css("left","0");
+  $(".overlay").css("display","block");
+})
+$(".btn-back-icon").click(function() {
+  $(".sidebar-men").css("left","-360px");
+  $(".sidebar-women").css("left","-360px");
+})
+
+$(".btn-back-icon-1").click(function() {
+  $(".sidebar-brand").css("left","-360px");
+  $(".sidebar-brand-women").css("left","-360px");
+})
+
+$(".brand-menu").click(function() {
+  $(".sidebar-brand").css("left","0");
+  $(".sidebar-brand-women").css("left","0");
+})
+
+// Sidebar-table,mobile
+$(".subnav .menu-item-tablet:nth-child(1)").click(function() {
+  $(".sidebar-men").css("left","0");
+  $(".overlay").css("display","block");
+})
+$(".subnav .menu-item-tablet:nth-child(2)").click(function() {
+  $(".sidebar-women").css("left","0");
+  $(".overlay").css("display","block");
+})
 // Đăng nhập, đăng xuất
 
 $(".icon-user").click(function () {
@@ -36,7 +107,22 @@ $(document).ready(function () {
 
 var owl = $(".owl-carousel");
 owl.owlCarousel({
-  items: 5,
+  responsiveClass:true,
+    responsive:{
+        0:{
+            items:2,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    },
   loop: true,
   margin: 10,
   autoplay: true,
